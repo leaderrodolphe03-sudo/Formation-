9268e5b89b47f39da5aa9045548bf9ff4e159d4c
Rodolphe <leaderrodolphe@gmail.com>
2026-09-23 07:57:12 -0600

    l'exercice de pytnon pour calcul

---
+++ exo3.py
@@ -0,0 +1,19 @@
+print ("Entrez le prix unitaire de l'acticle : ")
+pn = int(input())
+print (" Entrez la quantité achetée : ")
+qua = int(input())
+print ("Entrez le taux de remise")
+txr = int(input())
+
+#calcule du montant brut
+mntbr = int(pn*qua)
+print (" Le montant brut est : ", mntbr)
+
+#calcule de la remise 
+remise = int(mntbr * txr / 100)
+print (" La remise est : ", remise)
+
+#calcule du montant net
+mntnet = int(mntbr - remise)
+print (" Le montant net est : ", mntnet)
+
